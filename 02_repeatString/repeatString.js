22fa0a0e36c781c000eda02b773400f2ca1f6604
blackjackblack <basem.alsiddiq@gmail.com>
2021-09-05 19:56:25 +1000

    Finished 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    temp = string;
+    let negative = "ERROR";
+    if (num === 0) {
+        return "";
+    }
+    if (num < 0) {
+        return negative;
+    }
+    
+    for (i = 1; i < num; i++) {
+        string = string + temp;
+    }
+    return string;
 };
 
 module.exports = repeatString;
